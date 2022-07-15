83e7ffb21294ee2bdec4c91fe1be7765df35dd72
Bair Arshaev <ars-bair@yandex.ru>
2022-07-16 01:22:31 +0300

    Edit code 1

---
+++ src/Dictionary.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+
+public class Dictionary implements Function<String, Set<String>> {
+
+
+    @Override
+    public Set<String> apply(String s) {
+
+        return null;
+    }
+}
