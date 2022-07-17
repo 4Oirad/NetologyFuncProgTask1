3c2bfc2b722d16625e8bcd4cca63db8185197d78
Bair Arshaev <ars-bair@yandex.ru>
2022-07-17 23:29:49 +0300

    Edit code 3

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,20 +1,13 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Dictionary implements Function<String, Set<String>> {
 
     @Override
-    public Set<String> apply(String s) {
-
+    public TreeSet<String> apply(String s) {
         return Arrays.stream(s.split(" "))
                 .filter(x -> !x.equals(""))
-                .sorted(Comparator.naturalOrder())
-                .collect(Collectors.toSet());
-                //.forEach(x -> treeSet.add(x));
-
+                .collect(Collectors.toCollection(TreeSet::new));
     }
 }
